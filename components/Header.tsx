39d71b3ec526502bca44da7f4526ce18f7acaff8
Kaíque Zimerer <kaiquezimerer@outlook.com>
2022-01-26 18:36:28 -0300

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import {
+  Center,
+  Heading,
+} from '@chakra-ui/react';
+
+// Header
+export default function Header() {
+  return (
+    <Center py="8">
+      <Heading as="h1">
+        <Image
+          src="/img/logo.svg"
+          alt="worldtrip"
+          width="185px"
+          height="45px"
+        />
+      </Heading>
+    </Center>
+  );
+}
