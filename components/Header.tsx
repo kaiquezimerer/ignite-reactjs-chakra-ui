1658212208766ffc933c614362719afb81d3f3ab
Kaíque Zimerer <kaiquezimerer@outlook.com>
2022-01-27 20:45:45 -0300

    Create continent page (dynamic route) + Header back button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,49 @@
+import Link from 'next/link';
 import Image from 'next/image';
-import { Center, Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { Box, Center, Heading, IconButton, Tooltip } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
+interface HeaderProps {
+  hasButton?: boolean;
+}
 
 // Header
-export default function Header() {
+export default function Header({ hasButton = false }: HeaderProps) {
+  const router = useRouter();
+  const { name } = router.query;
+
+  useEffect(() => {
+    name && console.log(name);
+  }, [name]);
+
   return (
-    <Center py="8">
-      <Heading as="h1">
-        <Image
-          src="/img/logo.svg"
-          alt="worldtrip"
-          width="185px"
-          height="45px"
-        />
-      </Heading>
-    </Center>
+    <Box py="8" maxW={1160} mx="auto">
+      {
+        hasButton && (
+          <Link href="/" passHref>
+            <Tooltip label="Voltar">
+              <IconButton
+                variant="unstyled"
+                aria-label="Voltar"
+                position="absolute"
+                icon={<ChevronLeftIcon w={30} h={30} />}
+              />
+            </Tooltip>
+          </Link>
+        )
+      }
+      <Center>
+        <Heading as="h1">
+          <Image
+            src="/img/logo.svg"
+            alt="worldtrip"
+            width="185px"
+            height="45px"
+          />
+        </Heading>
+      </Center>
+    </Box>
   );
 }
