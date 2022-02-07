8b6110d24bbe5524ca6adac84ed66e40472be2c1
Kaíque Zimerer <kaiquezimerer@outlook.com>
2022-02-07 20:09:48 -0300

    Add responsive styles in continent page (mobile)

--- pages/continente/[name].tsx
+++ pages/continente/[name].tsx
@@ -11,6 +11,7 @@ import {
   Heading,
   Tooltip,
   Center,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { WarningIcon } from '@chakra-ui/icons';
 
@@ -20,13 +21,18 @@ export default function Continent() {
   const router = useRouter();
   const { name } = router.query;
 
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <>
       <Header hasButton />
       <Box
         as="header"
         w="100%"
-        h={500}
+        h={isWideVersion? 500 : 150}
         backgroundImage="url(/img/banner-continent.png)"
         backgroundPosition="center"
         backgroundRepeat="no-repeat"
@@ -37,14 +43,15 @@ export default function Continent() {
             as="h2"
             size="xl"
             position="absolute"
+            left="5px"
             bottom="60px"
           >
             {name}
           </Heading>
         </Box>
       </Box>
-      <Flex maxW={1240} mx="auto" my="80px" justify="space-between" flexWrap="wrap">
-        <Text maxW={600} fontSize="xl" textAlign="justify" lineHeight="36px">
+      <Flex maxW={1240} mx="auto" my="80px" px="15px" justify="space-between" flexWrap="wrap">
+        <Text maxW={600} fontSize={["md", "xl"]} textAlign="justify" lineHeight="36px">
           A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
         </Text>
         <UnorderedList
@@ -53,16 +60,17 @@ export default function Continent() {
           justifyContent="space-between"
           alignItems="center"
           w={480}
+          mt="16px"
         >
-          <ListItem>
+          <ListItem mx={2}>
             <Text fontSize="xl" fontWeight={600} textAlign="center">
               <Box fontSize="4xl" color="yellow.800" fontWeight={700}>50</Box> países</Text>
           </ListItem>
-          <ListItem>
+          <ListItem mx={2}>
             <Text fontSize="xl" fontWeight={600} textAlign="center">
               <Box fontSize="4xl" color="yellow.800" fontWeight={700}>60</Box> línguas</Text>
           </ListItem>
-          <ListItem>
+          <ListItem mx={2}>
             <Text fontSize="xl" fontWeight={600} textAlign="center">
               <Box fontSize="4xl" color="yellow.800" fontWeight={700}>27</Box> cidades +100
               <Tooltip label="Ver mais cidades">
@@ -76,8 +84,8 @@ export default function Continent() {
         <Heading as="h3" color="black.100" size="xl" fontWeight={500} mb="10">
           Cidades +100
         </Heading>
-        <SimpleGrid columns={[1, 2, 4]} spacing={12}>
-          <Box bg="white" w={256} h={279} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
+        <Flex flexWrap="wrap" justifyContent={isWideVersion ? 'space-between' : 'center'}>
+          <Box bg="white" w={256} h={279} mx={4} mb={4} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
             <Image src="/img/londres.jpg" alt="Londres" />
             <Flex h="106px" alignItems="center" py={4} px={6}>
               <Box w="50%" h="100%">
@@ -93,7 +101,7 @@ export default function Continent() {
               </Center>
             </Flex>
           </Box>
-          <Box bg="white" w={256} h={279} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
+          <Box bg="white" w={256} h={279} mx={4} mb={4} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
             <Image src="/img/paris.png" alt="Paris" />
             <Flex h="106px" alignItems="center" py={4} px={6}>
               <Box w="50%" h="100%">
@@ -109,7 +117,7 @@ export default function Continent() {
               </Center>
             </Flex>
           </Box>
-          <Box bg="white" w={256} h={279} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
+          <Box bg="white" w={256} h={279} mx={4} mb={4} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
             <Image src="/img/roma.png" alt="Roma" />
             <Flex h="106px" alignItems="center" py={4} px={6}>
               <Box w="50%" h="100%">
@@ -125,7 +133,7 @@ export default function Continent() {
               </Center>
             </Flex>
           </Box>
-          <Box bg="white" w={256} h={279} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
+          <Box bg="white" w={256} h={279} mx={4} mb={4} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
             <Image src="/img/praga.png" alt="Praga" />
             <Flex h="106px" alignItems="center" py={4} px={6}>
               <Box w="50%" h="100%">
@@ -141,7 +149,7 @@ export default function Continent() {
               </Center>
             </Flex>
           </Box>
-          <Box bg="white" w={256} h={279} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
+          <Box bg="white" w={256} h={279} mx={4} mb={4} borderRadius="4px" border=" 1px solid rgba(255, 186, 8, 0.5);">
             <Image src="/img/amsterda.png" alt="Amsterdã" />
             <Flex h="106px" alignItems="center" py={4} px={6}>
               <Box w="50%" h="100%">
@@ -157,7 +165,7 @@ export default function Continent() {
               </Center>
             </Flex>
           </Box>
-        </SimpleGrid>
+        </Flex>
       </Box>
     </>
   );
