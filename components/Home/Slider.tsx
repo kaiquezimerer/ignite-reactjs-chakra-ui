56dc6ace0d488a640e30c1084b7338347deb14fc
Kaíque Zimerer <kaiquezimerer@outlook.com>
2022-02-15 23:24:47 -0300

    Create API route for continents list

--- components/Home/Slider.tsx
+++ components/Home/Slider.tsx
@@ -3,12 +3,42 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Box, Heading, Flex } from '@chakra-ui/react';
+import { Box, Heading, Flex, Center, Spinner } from '@chakra-ui/react';
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
 
 import styles from 'styles/swiper.module.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Slider() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [continents, setContinents] = useState<any>([]);
+
+  async function loadContinentsList() {
+    setIsLoading(true);
+
+    try {
+      const { data } = await axios.get('http://localhost:3000/api/continents');
+      setContinents(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadContinentsList();
+  }, [])
+
+  if (isLoading) {
+    return (
+      <Center mt="40">
+        <Spinner color="gray.800" />
+      </Center>
+    );
+  } 
+
   return (
     <Box bg="gray" w="100%" h={[375, 450]} my={['5', '12']}>
       <Swiper cssMode={true}
@@ -19,150 +49,34 @@ export default function Slider() {
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
         className={styles.swiper}
       >
-        <SwiperSlide>
-          <Link href="/continente/america-do-norte" passHref>
-            <Flex
-              bgImg="url(/img/europa.png)"
-              justifyContent="center"
-              align="center"
-              flexDirection="column"
-              w="100%"
-              h="100%"
-            >
-              <Heading
-                as="h2"
-                mb="4"
-                fontSize={['24px', '48px']}
-                fontWeight={700}
-              >
-                América do Norte
-              </Heading>
-              <Heading as="h3" fontSize={['14px', '24px']}>
-                A terra do progresso e da oportunidade.
-              </Heading>
-            </Flex>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href="/continente/america-do-sul" passHref>
-            <Flex
-              bgImg="url(/img/europa.png)"
-              justifyContent="center"
-              align="center"
-              flexDirection="column"
-              w="100%"
-              h="100%"
-            >
-              <Heading
-                as="h2"
-                mb="4"
-                fontSize={['24px', '48px']}
-                fontWeight={700}
-              >
-                América do Sul
-              </Heading>
-              <Heading as="h3" fontSize={['14px', '24px']}>
-                Diversidade e paraíso natural.
-              </Heading>
-            </Flex>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href="/continente/asia" passHref>
-            <Flex
-              bgImg="url(/img/europa.png)"
-              justifyContent="center"
-              align="center"
-              flexDirection="column"
-              w="100%"
-              h="100%"
-            >
-              <Heading
-                as="h2"
-                mb="4"
-                fontSize={['24px', '48px']}
-                fontWeight={700}
-              >
-                Ásia
-              </Heading>
-              <Heading as="h3" fontSize={['14px', '24px']}>
-                A terra da sabedoria e da cultura.
-              </Heading>
-            </Flex>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href="/continente/africa" passHref>
-            <Flex
-              bgImg="url(/img/europa.png)"
-              justifyContent="center"
-              align="center"
-              flexDirection="column"
-              w="100%"
-              h="100%"
-            >
-              <Heading
-                as="h2"
-                mb="4"
-                fontSize={['24px', '48px']}
-                fontWeight={700}
-              >
-                África
-              </Heading>
-              <Heading as="h3" fontSize={['14px', '24px']}>
-                O berço da humanidade.
-              </Heading>
-            </Flex>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href="/continente/europa" passHref>
-            <Flex
-              bgImg="url(/img/europa.png)"
-              justifyContent="center"
-              align="center"
-              flexDirection="column"
-              w="100%"
-              h="100%"
-            >
-              <Heading
-                as="h2"
-                mb="4"
-                fontSize={['24px', '48px']}
-                fontWeight={700}
-              >
-                Europa
-              </Heading>
-              <Heading as="h3" fontSize={['14px', '24px']}>
-                O continente mais antigo.
-              </Heading>
-            </Flex>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href="/continente/oceania" passHref>
-            <Flex
-              bgImg="url(/img/europa.png)"
-              justifyContent="center"
-              align="center"
-              flexDirection="column"
-              w="100%"
-              h="100%"
-            >
-              <Heading
-                as="h2"
-                mb="4"
-                fontSize={['24px', '48px']}
-                fontWeight={700}
-              >
-                Oceania
-              </Heading>
-              <Heading as="h3" fontSize={['14px', '24px']}>
-                O continente dos arquipélogos.
-              </Heading>
-            </Flex>
-          </Link>
-        </SwiperSlide>
+        {
+          continents?.map((continent) => (
+            <SwiperSlide key={continent.name}>
+              <Link href={`/continente/${continent.slug}`} passHref>
+                <Flex
+                  bgImg={`url(${continent.bgUrl})`}
+                  justifyContent="center"
+                  align="center"
+                  flexDirection="column"
+                  w="100%"
+                  h="100%"
+                >
+                  <Heading
+                    as="h2"
+                    mb="4"
+                    fontSize={['24px', '48px']}
+                    fontWeight={700}
+                  >
+                    {continent.name}
+                  </Heading>
+                  <Heading as="h3" fontSize={['14px', '24px']}>
+                    {continent.description}
+                  </Heading>
+                </Flex>
+              </Link>
+            </SwiperSlide>
+          ))
+        }
       </Swiper>
     </Box>
   );
---
+++ pages/api/continents.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const CONTINENTS_DATA = [
+  { name: 'América do Norte', slug: 'america-do-norte', description: 'A terra do progresso e da oportunidade.', bgUrl: '/img/europa.png' },
+  { name: 'América do Sul',slug: 'america-do-sul', description: 'Diversidade e paraíso natural.', bgUrl: '/img/europa.png' },
+  { name: 'Ásia',slug: 'asia', description: 'A terra da sabedoria e da cultura.', bgUrl: '/img/europa.png' },
+  { name: 'África',slug: 'africa', description: 'O berço da humanidade.', bgUrl: '/img/europa.png' },
+  { name: 'Europa',slug: 'europa', description: 'O continente mais antigo.', bgUrl: '/img/europa.png' },
+  { name: 'Oceania',slug: 'oceania', description: 'O continente dos arquipélogos.', bgUrl: '/img/europa.png' },
+];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    res.status(200).json(CONTINENTS_DATA)
+  } else {
+    res.status(400);
+  }
+}
